2dd9829c721af028f2b1f063301a82da3701fae0
User <User@PC>
2015-07-21 17:24:41 +0300

    comit

--- JPA/src/jpa/test/Main.java
+++ JPA/src/jpa/test/Main.java
@@ -1,5 +1,7 @@
 package jpa.test;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -7,14 +9,39 @@ import javax.persistence.Persistence;
 public class Main {
 
 	public static void main(String[] args) {
-
+      
 		   
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA");
 		EntityManager em = emf.createEntityManager();
-        User user = new User("Vasa", 20);
-        em.getTransaction().begin();
+		Service serv = new Service(em);
+		Role r = new Role("user");
+		   em.getTransaction().begin();
+	        em.persist(r);
+        User user = new User("Vasa", 20);;
+        User user1 = new User("Peta", 32);
+        User user2 = new User("Pipa", 42);
+        User user3 = new User("Mupa", 21);
+        user.setRole(r);
+       /* user1.setRole(r);
+        user2.setRole(r);
+        user3.setRole(r);
+        em.persist(r);*/
         em.persist(user);
+    /*    em.persist(user1);
+        em.persist(user2);
+        em.persist(user3);*/
+       
         em.getTransaction().commit();
-
+ /*       serv.save(user);
+        serv.save(user1);
+        serv.save(user2);
+        serv.save(user3);*/
+        List<User> list;
+        list= em.createNamedQuery("User.getAll", User.class).getResultList();
+        System.out.println(list);
+        System.out.println(serv.getbyName("vasa").toString());
+        System.out.println(serv.getbyName("pupa").toString());
+        em.close();
+        emf.close();
 	}
 }
--- JPA/src/jpa/test/User.java
+++ JPA/src/jpa/test/User.java
@@ -1,19 +1,43 @@
 package jpa.test;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQueries({
+	@NamedQuery(name="User.getAll", query="SELECT u From User u"),
+	@NamedQuery(name="User.getbyName", query="SELECT u From User u where u.name=:name")
+
+})
 public class User {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+	
+
 	private String name;
 	private int age;
 	
+	@ManyToOne
+	private Role role;
+	
+	
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
 	public User(String name, int age) {
 		super();
 		this.name = name;
@@ -36,5 +60,10 @@ public class User {
 	}
 	public Long getId() {
 		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age + "]";
 	}
 }
